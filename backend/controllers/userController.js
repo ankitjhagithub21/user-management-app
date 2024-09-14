47b5da8fd25d43140ebdad5ae1d3360a2ba5f132
ankitjhagithub21 <ajha45075@gmail.com>
2024-09-14 10:53:40 +0530

    created controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,21 @@
+const User = require("../model/user")
 const createUser = async(req,res) =>{
     try{
 
+        const {name,phone} = req.body;
+        
+        const newUser = new User({
+            name,
+            phone
+        })
+
+        await newUser.save()
+
+        res.status(201).json({
+            success:true,
+            message:"User created successfully.",
+            user:newUser
+        })
     }catch(error){
         res.status(500).json({
             success:false,
@@ -12,6 +27,22 @@ const createUser = async(req,res) =>{
 const deleteUser = async(req,res) =>{
     try{
 
+        const {userId} = req.params;
+
+        const user = await User.findByIdAndDelete(userId)
+
+        if(!user){
+           return res.status(404).json({
+                success:false,
+                message:"User not found."
+            })
+        }
+
+        res.status(200).json({
+            success:true,
+            message:"User deleted successfully."
+        })
+
     }catch(error){
         res.status(500).json({
             success:false,
@@ -22,6 +53,28 @@ const deleteUser = async(req,res) =>{
 
 const updateUser = async(req,res) =>{
     try{
+        const {userId} = req.params;
+
+        const {name,phone} = req.body;
+
+        const user = await User.findByIdAndUpdate(userId,{
+            name,
+            phone
+        },{new:true})
+        
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:"User not found."
+            })     
+        }
+
+        res.status(200).json({
+            success:true,
+            message:"User updated successfully.",
+            user
+        })
+
 
     }catch(error){
         res.status(500).json({
@@ -33,6 +86,10 @@ const updateUser = async(req,res) =>{
 
 const getUsers = async(req,res) =>{
     try{
+
+        const users = await User.find({})
+
+        res.status(200).json(users)
 
     }catch(error){
         res.status(500).json({
